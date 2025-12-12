69522d21e645974b718cb79acdd19a77a4e66a58
anamolhasan <mdanamolhasan1998@gmail.com>
2025-12-12 22:27:48 +0600

    auth added

--- src/modules/users/user.route.ts
+++ src/modules/users/user.route.ts
@@ -1,14 +1,16 @@
 import { Router } from "express";
 import { userController } from "./user.controller.js";
+import auth from "../../middleware/auth.js";
+import { role } from "../../utils/role.js";
 
 
 
 
 const router = Router();
 
-router.get('/', userController.getAllUser)
-router.put('/:id', userController.updateUser)
-router.delete('/:id', userController.deleteUser)
+router.get('/',auth(role.admin),  userController.getAllUser)
+router.put('/:id',auth(role.admin), userController.updateUser)
+router.delete('/:id',auth(role.admin), userController.deleteUser)
 
 
 export const userRouter = router
---
+++ src/utils/lowercaseEmail.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from "express";
+
+
+export const lowercaseEmail = (
+    req:Request,
+    res:Response,
+    next:NextFunction
+) => {
+    if(req.body.email) {
+        req.body.email = req.body.email.toLowerCase()
+    }
+    next()
+}
